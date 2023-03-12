5c9184b68150949a1fe1ebaf2c2ce19091af8b4b
yujen77300 <yujenhuang24@gmail.com>
2023-03-13 00:20:26 +0800

    first commit

---
+++ data/data.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify
+import mysql.connector
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+with open("data/taipei-attractions.json", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+spotsInfo = data["result"]["results"]
+
+
+taipeiPool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="connectionPool",
+    pool_size=20,
+    pool_reset_session=True,
+    host='localhost',
+    database=os.getenv('DB_NAME'),
+    user=os.getenv('DB_ACCOUNT'),
+    password=os.getenv('DB_PW')
+  )
+
+mydb = taipeiPool.get_connection()
+cur = mydb.cursor()
+
+for i in spotsInfo:
+    cur.execute("INSERT IGNORE INTO spot_info(_id, CAT, MEMO_TIME, MRT, SERIAL_NO, address, avBegin, avEnd, date, description, direction, idpt, langinfo, latitude, longitude, name, rate) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                (i["_id"], i["CAT"], i["MEMO_TIME"], i["MRT"], i["SERIAL_NO"], i["address"], (i["avBegin"]).replace('/', '-'), (i["avEnd"]).replace('/', '-'), (i["date"]).replace('/', '-'), i["description"], i["direction"], i["idpt"], i["langinfo"], i["latitude"], i["longitude"], i["name"], i["rate"]))
+mydb.commit()
+
+
+spotIdAndImage = {}
+
+for i in spotsInfo:
+    spotImageAddress = []
+    spotImageAddressWithJpgPng = []
+    _id = i["_id"]
+    spotImageAddress = i["file"].split('https://')
+
+    spotImageAddress.pop(0)
+    spotImageAddressWithJpgPng = spotImageAddress
+    for address in spotImageAddress[:]:
+ 
+        if (address[-3:]).upper() == "PNG" or (address[-3:]).upper() == "JPG":
+            pass
+        else:
+
+            spotImageAddressWithJpgPng.remove(address)
+
+    spotIdAndImage[_id] = spotImageAddressWithJpgPng
+
+
+
+for i in spotsInfo:
+    for image in spotIdAndImage[i["_id"]]:
+        cur.execute("INSERT INTO image(name, _id, file) VALUES(%s,%s,%s)",
+                    (i["name"], i["_id"], image))
+mydb.commit()
+cur.close()
+mydb.close()
+
+
+app.run(port=5000)
