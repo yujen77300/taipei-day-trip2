899bc500fe80f31277618e960c9e51da86109a5a
yujen77300 <yujenhuang24@gmail.com>
2023-03-13 20:04:25 +0800

    style:index.html

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import *
+
+
+app = Flask(
+    __name__,
+    static_folder="public",
+    static_url_path="/"
+)
+
+app.config["JSON_AS_ASCII"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+app.run(port=3000)
